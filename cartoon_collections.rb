21e0a144fccf411f5087e97661f003c4b2fddef0
Irvin Natt <ipnatt@gmail.com>
2020-11-09 00:05:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,8 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  valid_calls.find do |element|
+  valid_calls.map do |element|
+    element.find
     
   end
   
