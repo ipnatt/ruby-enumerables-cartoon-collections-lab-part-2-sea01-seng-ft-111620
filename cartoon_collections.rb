40bf1d04a11d9c83fcddcc423212cb97d85e83a0
Irvin Natt <ipnatt@gmail.com>
2020-11-09 00:01:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.include?(valid_calls)
-  valid_calls.find
+  valid_calls.find(valid_calls)
   end
   
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
